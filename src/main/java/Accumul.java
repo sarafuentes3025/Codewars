2625c247ecbe8939f756f651f7d10b57e4258a7f
Sara Fuentes <sara.fuentes3025@gmail.com>
2022-02-12 21:04:22 -0600

    Refactor Accumul class

--- src/main/java/Accumul.java
+++ src/main/java/Accumul.java
@@ -1,14 +1,14 @@
 public class Accumul {
   public static String accum(String s) {
     char[] chars = s.toCharArray();
-    String result = Character.toString(chars[0]);
+    StringBuilder result = new StringBuilder(Character.toString(chars[0]));
 
     for (int i = 1; i < s.length(); i++) {
-      result += "-" + Character.toString(chars[i]).toUpperCase();
+      result.append("-").append(Character.toString(chars[i]).toUpperCase());
       for (int j = 0; j < i; j++) {
-        result += Character.toLowerCase(chars[i]);
+        result.append(Character.toLowerCase(chars[i]));
       }
     }
-    return result;
+    return result.toString();
   }
 }
